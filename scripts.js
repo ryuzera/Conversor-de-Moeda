aedbb00e665fd3cb4c73c40405216eb6081ce1a6
ryuzera <ryugfx2@gmail.com>
2024-11-15 11:15:26 -0300

    Identificando a moeda

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+// Cotação de moedas do dia 15/11/2024
+const USD = 5.79
+const EUR = 6.12
+const CAD = 4.13
+const AUD = 3.75
+const GBP = 7.35
+const JPY = 0.037
+
+
 // Obtendo os elementos do formulário
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
@@ -14,5 +23,35 @@ amount.addEventListener("input", () => {
 // Capturando o evento de submit no forumlário
 form.onsubmit = (event) => {
     event.preventDefault()
-    console.log(currency.value) // Pegando o value = USD, BRL, AUD
+
+    switch (currency.value) {
+        case "USD":
+            convertCurrency(amount.value, USD, "US$")
+                break
+        
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+        
+        case "CAD":
+            convertCurrency(amount.value, CAD, "C$")
+                break
+        
+        case "AUD":
+            convertCurrency(amount.value, AUD, "AU$")
+            break
+
+        case "GPB":
+            convertCurrency(amount.value, GPB, "£")
+            break
+            
+        case "JPY":
+            convertCurrency(amount.value, JPY, "¥")
+            break
+    }
+}
+
+// Função para converter a moeda
+function convertCurrency(amount, price, symbol) {
+    console.log(amount, price, symbol)
 }
